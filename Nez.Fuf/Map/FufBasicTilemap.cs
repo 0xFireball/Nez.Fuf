1175b5e21d179fbf06e37129e5bae87ba7eac757
0xFireball <0xFireball@outlook.com>
2017-08-05 23:32:40 -0700

    add tool to load tilemap from array

---
+++ Nez.Fuf/Map/FufBasicTilemap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Xna.Framework.Graphics;
+using Nez.Tiled;
+
+namespace Nez.Fuf.Map
+{
+    /// <summary>
+    /// A highly simplified tilemap that can be loaded from an array.
+    /// </summary>
+    public class FufBasicTilemap : TiledMap
+    {
+        public FufBasicTilemap(
+            int firstGid,
+            int width,
+            int height,
+            int tileWidth,
+            int tileHeight,
+            TiledMapOrientation orientation = TiledMapOrientation.Orthogonal) : base(
+            firstGid,
+            width,
+            height,
+            tileWidth,
+            tileHeight
+        )
+        {
+        }
+
+        public FufBasicTilemap loadFromArray(string name, int[] tileData, int width, int height, TiledTileset tileset, int tileWidth,
+            int tileHeight)
+        {
+            var tiles = tileData.Select(x => new TiledTile(1) { tileset = tileset }).ToArray();
+            var tileLayer = createTileLayer(name, width, height, tiles);
+            
+            return this;
+        }
+    }
+}
