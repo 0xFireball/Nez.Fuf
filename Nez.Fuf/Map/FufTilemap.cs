c640a3bb85a33a511a58bcc7b67a86a6f33e7ae3
0xFireball <0xFireball@outlook.com>
2017-08-05 23:25:38 -0700

    add map stuff

---
+++ Nez.Fuf/Map/FufTilemap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Nez.Tiled;
+
+namespace Nez.Fuf.Map
+{
+    public class FufTilemap : TiledMap
+    {
+        public FufTilemap(
+            int firstGid,
+            int width,
+            int height,
+            int tileWidth,
+            int tileHeight,
+            TiledMapOrientation orientation = TiledMapOrientation.Orthogonal) : base(
+                firstGid,
+                width,
+                height,
+                tileWidth,
+                tileHeight
+            )
+        {
+        }
+    }
+}
