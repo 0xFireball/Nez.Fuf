7f95a2b94e0e3798e27bc5ebfcbf8e6763ef7d44
0xFireball <0xFireball@outlook.com>
2017-08-06 10:56:17 -0700

    clean up fuf tilemap

--- Nez.Fuf/Map/FufBasicTilemap.cs
+++ Nez.Fuf/Map/FufBasicTilemap.cs
@@ -26,13 +26,13 @@ namespace Nez.Fuf.Map
         {
         }
 
-        public FufBasicTilemap loadFromArray(string name, int[] tileData, int width, int height, TiledTileset tileset, int tileWidth,
-            int tileHeight)
+        public TiledLayer loadTileLayerFromArray(string name, int[] tileData, int width, int height,
+            TiledTileset tileset)
         {
-            var tiles = tileData.Select(x => new TiledTile(x) { tileset = tileset }).ToArray();
+            var tiles = tileData.Select(x => new TiledTile(x) {tileset = tileset}).ToArray();
             var tileLayer = createTileLayer(name, width, height, tiles);
-            
-            return this;
+
+            return tileLayer;
         }
     }
 }
