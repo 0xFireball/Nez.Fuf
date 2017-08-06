47e9ecd6862d59c2d49ec0c578e21c13b8f16096
0xFireball <0xFireball@outlook.com>
2017-08-05 17:28:34 -0700

    add logic inputs

---
+++ Nez.Fuf/Input/Virtual/VirtualAxis.cs
@@ -0,0 +1,12 @@
+﻿namespace Nez.Fuf
+{
+    public class VirtualAxis
+    {
+        public class LogicAxis : Nez.VirtualAxis.Node
+        {
+            public override float value => Value;
+
+            public float Value { get; set; }
+        }
+    }
+}
---
+++ Nez.Fuf/Input/Virtual/VirtualButton.cs
@@ -0,0 +1,14 @@
+﻿namespace Nez.Fuf
+{
+    public class VirtualButton
+    {
+        public class LogicButton : Nez.VirtualButton.Node
+        {
+            public override bool isDown => false;
+            public override bool isPressed => Pressed;
+            public override bool isReleased => !Pressed;
+            
+            public bool Pressed { get; set; }
+        }
+    }
+}
