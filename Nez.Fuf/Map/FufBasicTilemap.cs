8fc0b7a311334a569238c35ec25e9721f8ed9e44
0xFireball <0xFireball@outlook.com>
2017-08-06 17:29:38 -0700

    Revert "clean up fuf tilemap"
    This reverts commit 7f95a2b94e0e3798e27bc5ebfcbf8e6763ef7d44.

--- Nez.Fuf/Map/FufBasicTilemap.cs
+++ Nez.Fuf/Map/FufBasicTilemap.cs
@@ -26,13 +26,13 @@ namespace Nez.Fuf.Map
         {
         }
 
-        public TiledLayer loadTileLayerFromArray(string name, int[] tileData, int width, int height,
-            TiledTileset tileset)
+        public FufBasicTilemap loadFromArray(string name, int[] tileData, int width, int height, TiledTileset tileset, int tileWidth,
+            int tileHeight)
         {
-            var tiles = tileData.Select(x => new TiledTile(x) {tileset = tileset}).ToArray();
+            var tiles = tileData.Select(x => new TiledTile(x) { tileset = tileset }).ToArray();
             var tileLayer = createTileLayer(name, width, height, tiles);
-
-            return tileLayer;
+            
+            return this;
         }
     }
 }
