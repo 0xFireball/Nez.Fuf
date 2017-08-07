48c4c5b09d6eb4240d63c0470e7a7c774b6627a3
0xFireball <0xFireball@outlook.com>
2017-08-07 11:41:44 -0700

    abstract some texture loading

---
+++ Nez.Fuf/Sprites/FufSprite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
+using Nez;
+using Nez.Sprites;
+using Nez.Textures;
+
+namespace Nez.Fuf.Sprites
+{
+    public class FufSprite : Component
+    {
+        protected Texture2D _texture;
+        protected List<Subtexture> _subtextures;
+
+        public void loadGraphic(string graphicAsset, bool animated = false, int width = 0, int height = 0)
+        {
+            _texture = entity.scene.content.Load<Texture2D>(graphicAsset);
+
+            if (animated)
+            {
+                _subtextures = Subtexture.subtexturesFromAtlas(_texture, width, height);
+            }
+            else
+            {
+                entity.addComponent(new Sprite(_texture));
+            }
+        }
+    }
+}
