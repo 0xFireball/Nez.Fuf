3a45503ea6a23f64714865f9504ee97c528cb46f
0xFireball <0xFireball@outlook.com>
2017-08-07 22:41:02 -0700

    add projectilebody

---
+++ Nez.Fuf/Physics/ProjectileBody.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Nez.Fuf.Physics
+{
+    public class ProjectileBody : Component, IUpdatable
+    {
+        private ProjectileMover _mover;
+
+        public Vector2 velocity = Vector2.Zero; 
+
+        public override void initialize()
+        {
+            base.initialize();
+            
+            _mover = entity.addComponent<ProjectileMover>();
+        }
+
+        public void update()
+        {
+            if (_mover.move(velocity * Time.deltaTime))
+            {
+                onProjectileHit();
+            }
+        }
+
+        public virtual void onProjectileHit()
+        {
+            // destroy
+            entity.destroy();
+        }
+    }
+}
