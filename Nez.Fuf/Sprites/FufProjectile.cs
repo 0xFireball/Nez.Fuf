39abe6e42bed3c6daa83ab1d3b26eb0e5d394e4f
0xFireball <0xFireball@outlook.com>
2017-08-11 23:39:26 -0700

    use new projectile type

---
+++ Nez.Fuf/Sprites/FufProjectile.cs
@@ -0,0 +1,10 @@
+﻿using Nez.Fuf.Physics;
+
+namespace Nez.Fuf.Sprites
+{
+    public class FufProjectile : Component
+    {
+        public Component owner;
+        public ProjectileBody body;
+    }
+}
