08bb0457979c2e331d5d0d3cff97da29efaeb4cd
0xFireball <0xFireball@outlook.com>
2017-08-12 10:34:19 -0700

    make animation publicly accessible

--- Nez.Fuf/Sprites/FufAnimatedSprite.cs
+++ Nez.Fuf/Sprites/FufAnimatedSprite.cs
@@ -7,9 +7,13 @@ namespace Nez.Fuf.Sprites
     {
         protected Sprite<TAnimation> animation;
 
+        public Sprite<TAnimation> Animation => animation;
+
         public override void loadGraphic(string graphicAsset, bool animated = false, int width = 0, int height = 0)
         {
-            if (!animated) throw new ArgumentOutOfRangeException(nameof(animated), "animated sprites must have `animated` set to true in loadGraphic()");
+            if (!animated)
+                throw new ArgumentOutOfRangeException(nameof(animated),
+                    "animated sprites must have `animated` set to true in loadGraphic()");
             base.loadGraphic(graphicAsset, animated, width, height);
 
             animation = entity.addComponent(new Sprite<TAnimation>(_subtextures[0]));
