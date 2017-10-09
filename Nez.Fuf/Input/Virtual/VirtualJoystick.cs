ef0efe346f0113d9b07c1518a5a08b52ef9a965f
0xFireball <0xFireball@outlook.com>
2017-10-09 09:07:59 -0700

    add new art and ships

---
+++ Nez.Fuf/Input/Virtual/VirtualJoystick.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Nez.Fuf
+{
+    public class VirtualJoystick
+    {
+        public class LogicJoystick : Nez.VirtualJoystick.Node
+        {
+            public override Vector2 value { get; }
+
+            public Vector2 Value { get; set; }
+        }
+
+        public class MouseDirectionalJoystick : Nez.VirtualJoystick.Node
+        {
+            private Vector2 _mouseDirection;
+            public override Vector2 value => _mouseDirection;
+
+            public override void update()
+            {
+                var mouseInputCenter = new Vector2(Nez.Core.graphicsManager.PreferredBackBufferWidth,
+                                           Nez.Core.graphicsManager.PreferredBackBufferHeight) / 2;
+                _mouseDirection = Nez.Input.mousePosition - mouseInputCenter;
+            }
+        }
+    }
+}
