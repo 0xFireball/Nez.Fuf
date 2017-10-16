4b7b8f559f9bb91c8137ef2a36949117e0e753bf
0xFireball <0xFireball@outlook.com>
2017-10-16 08:00:25 -0700

    add FufBackdrop

---
+++ Nez.Fuf/Display/FufBackdrop.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Nez.Sprites;
+
+namespace Nez.Fuf.Display {
+    public class FufBackdrop : Component, IUpdatable {
+        private readonly Texture2D _texture;
+        public ScrollingSprite scrollingSprite;
+
+        public Vector2 scrollFactor = Vector2.One;
+
+        public FufBackdrop(Texture2D texture) {
+            _texture = texture;
+            setUpdateOrder(int.MaxValue);
+        }
+
+        public override void initialize() {
+            base.initialize();
+
+
+            scrollingSprite = entity.addComponent(new ScrollingSprite(_texture));
+            entity.updateOrder = int.MaxValue;
+        }
+
+        public void update() {
+            scrollingSprite.scrollX = (int) (entity.scene.camera.position.X * scrollFactor.X);
+            scrollingSprite.scrollY = (int) (entity.scene.camera.position.Y * scrollFactor.Y);
+            scrollingSprite.entity.position = entity.scene.camera.position;
+        }
+    }
+}
