d053f83b701a67139126ee79f391c14dc8679a09
0xFireball <0xFireball@outlook.com>
2017-10-24 21:57:34 -0700

    remove borderless func

--- Nez.Fuf/Platform/DesktopPlatform.cs
+++ Nez.Fuf/Platform/DesktopPlatform.cs
@@ -7,35 +7,5 @@ namespace Nez.Fuf.Platform {
         public DesktopPlatform(Core core) {
             _core = core;
         }
-
-        private bool _borderlessFullscreen = false;
-
-        public bool IsBorderlessFullscreen {
-            get => _borderlessFullscreen;
-            set {
-                if (value) EnterBorderlessFullscreen();
-                else ExitBorderlessFullscreen();
-                _borderlessFullscreen = value;
-            }
-        }
-
-        public void EnterBorderlessFullscreen() {
-            _core.Window.IsBorderless = true;
-            
-            // update backbuffer size
-            Core.graphicsManager.PreferredBackBufferWidth = Core.graphicsDevice.Adapter.CurrentDisplayMode.Width;
-            Core.graphicsManager.PreferredBackBufferHeight = Core.graphicsDevice.Adapter.CurrentDisplayMode.Height;
-
-            Core.graphicsManager.ApplyChanges();
-        }
-
-        public void ExitBorderlessFullscreen() {
-            _core.Window.IsBorderless = false;
-            // restore backbuffer size
-            Core.graphicsManager.PreferredBackBufferWidth = _core.defaultResolution.X;
-            Core.graphicsManager.PreferredBackBufferHeight = _core.defaultResolution.Y;
-
-            Core.graphicsManager.ApplyChanges();
-        }
     }
 }
