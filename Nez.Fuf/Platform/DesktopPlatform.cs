f740270fd6ce749b022b576e355014892512d090
0xFireball <0xFireball@outlook.com>
2017-10-24 21:47:35 -0700

    Borderless fullscreen support

---
+++ Nez.Fuf/Platform/DesktopPlatform.cs
@@ -0,0 +1,41 @@
+﻿using Nez.UI;
+
+namespace Nez.Fuf.Platform {
+    public class DesktopPlatform {
+        private readonly Core _core;
+
+        public DesktopPlatform(Core core) {
+            _core = core;
+        }
+
+        private bool _borderlessFullscreen = false;
+
+        public bool IsBorderlessFullscreen {
+            get => _borderlessFullscreen;
+            set {
+                if (value) EnterBorderlessFullscreen();
+                else ExitBorderlessFullscreen();
+                _borderlessFullscreen = value;
+            }
+        }
+
+        public void EnterBorderlessFullscreen() {
+            _core.Window.IsBorderless = true;
+            
+            // update backbuffer size
+            Core.graphicsManager.PreferredBackBufferWidth = Core.graphicsDevice.Adapter.CurrentDisplayMode.Width;
+            Core.graphicsManager.PreferredBackBufferHeight = Core.graphicsDevice.Adapter.CurrentDisplayMode.Height;
+
+            Core.graphicsManager.ApplyChanges();
+        }
+
+        public void ExitBorderlessFullscreen() {
+            _core.Window.IsBorderless = false;
+            // restore backbuffer size
+            Core.graphicsManager.PreferredBackBufferWidth = _core.defaultResolution.X;
+            Core.graphicsManager.PreferredBackBufferHeight = _core.defaultResolution.Y;
+
+            Core.graphicsManager.ApplyChanges();
+        }
+    }
+}
