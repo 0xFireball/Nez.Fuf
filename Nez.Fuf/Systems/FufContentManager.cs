969be32fe4e30fb7c3366956d23ec4be5ad03ff2
0xFireball <0xFireball@outlook.com>
2017-12-24 11:14:29 -0800

    code cleanup

--- Nez.Fuf/Systems/FufContentManager.cs
+++ Nez.Fuf/Systems/FufContentManager.cs
@@ -288,18 +288,16 @@ namespace Nez.Fuf.Systems {
         }
 
         /// <summary>
-		/// override that will load embedded resources if they have the "nez://" prefix
-		/// </summary>
-		/// <returns>The stream.</returns>
-		/// <param name="assetName">Asset name.</param>
-		protected override Stream OpenStream(string assetName)
-        {
-            if (assetName.StartsWith("nez://"))
-            {
+        /// override that will load embedded resources if they have the "nez://" prefix
+        /// </summary>
+        /// <returns>The stream.</returns>
+        /// <param name="assetName">Asset name.</param>
+        protected override Stream OpenStream(string assetName) {
+            if (assetName.StartsWith("nez://")) {
                 var assembly = ReflectionUtils.getAssembly(this.GetType());
 
 #if FNA
-				// for FNA, we will just search for the file by name since the assembly name will not be known at runtime
+// for FNA, we will just search for the file by name since the assembly name will not be known at runtime
 				foreach( var item in assembly.GetManifestResourceNames() )
 				{
 					if( item.EndsWith( assetName.Substring( assetName.Length - 20 ) ) )
