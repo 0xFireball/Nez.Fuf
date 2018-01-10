77c2aad5cddac9560b62d7bb5372748a48040099
0xFireball <0xFireball@outlook.com>
2018-01-09 20:59:39 -0800

    refactor sprite code

--- Nez.Fuf/Sprites/FufAnimatedSprite.cs
+++ Nez.Fuf/Sprites/FufAnimatedSprite.cs
@@ -1,19 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework.Graphics;
 using Nez.Sprites;
+using Nez.Textures;
 
 namespace Nez.Fuf.Sprites {
     public abstract class FufAnimatedSprite<TAnimation> : FufSprite, IUpdatable
         where TAnimation : struct, IComparable, IFormattable {
+        
+        protected readonly List<Subtexture> subtextures;
+        
         public Sprite<TAnimation> animation { get; private set; }
+
+        public Direction facing { get; set; }
 
         private bool _flipX;
         private bool _flipY;
 
-        protected FufAnimatedSprite(Texture2D texture, bool animated = false, int width = 0, int height = 0) : base(
-            texture, animated, width, height) {
-            animation = entity.addComponent(new Sprite<TAnimation>(_subtextures[0]));
+        /// <summary>
+        /// Create an animated sprite with the specified frame size
+        /// </summary>
+        /// <param name="texture"></param>
+        /// <param name="width">The frame width</param>
+        /// <param name="height">The frame height</param>
+        protected FufAnimatedSprite(Texture2D texture, int width, int height) : base(
+            texture) {
+            animation = entity.addComponent(new Sprite<TAnimation>(subtextures[0]));
             sprite = animation;
+
+            subtextures = Subtexture.subtexturesFromAtlas(base.texture, width, height);
         }
 
         public void setFacingFlip(bool flipX, bool flipY) {
--- Nez.Fuf/Sprites/FufSprite.cs
+++ Nez.Fuf/Sprites/FufSprite.cs
@@ -5,21 +5,14 @@ using Nez.Textures;
 
 namespace Nez.Fuf.Sprites {
     public abstract class FufSprite : Component {
-        protected Texture2D _texture;
+        protected readonly Texture2D texture;
         public Sprite sprite;
-        protected List<Subtexture> _subtextures;
 
-        public Direction facing { get; set; }
+        public FufSprite(Texture2D texture) {
+            this.texture = texture;
 
-        public FufSprite(Texture2D texture, bool animated = false, int width = 0, int height = 0) {
-            _texture = texture;
-
-            if (animated) {
-                _subtextures = Subtexture.subtexturesFromAtlas(_texture, width, height);
-            } else {
-                sprite = new Sprite(_texture);
-                entity.addComponent(sprite);
-            }
+            sprite = new Sprite(this.texture);
+            entity.addComponent(sprite);
         }
     }
 }
