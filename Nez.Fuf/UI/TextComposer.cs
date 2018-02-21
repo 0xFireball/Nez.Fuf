70da924766dba854567571c8b6796a6614fd6536
0xFireball <0xFireball@outlook.com>
2018-02-20 18:21:29 -0800

    more flexibility to text composition

--- Nez.Fuf/UI/TextComposer.cs
+++ Nez.Fuf/UI/TextComposer.cs
@@ -2,21 +2,25 @@
 
 namespace Nez.Fuf.UI {
     public class TextComposer {
-        public NezSpriteFont Font { get; }
-        public string Text { get; set; }
+        public string _text { get; }
+        public IFont _font { get; }
+        public float _scale { get; }
 
         private Entity _entity;
         private Text _textComponent;
 
         public Text TextComponent => _textComponent;
 
-        public TextComposer(NezSpriteFont font) {
-            Font = font;
+        public TextComposer(string text, IFont font, float scale) {
+            _text = text;
+            _font = font;
+            _scale = scale;
         }
 
         public TextComposer attach(Scene scene, Vector2 position, Color color, string entityName = null) {
             _entity = scene.createEntity(entityName, position);
-            _textComponent = _entity.addComponent(new Text(Font, Text, Vector2.Zero, color));
+            _textComponent = _entity.addComponent(new Text(_font, _text, Vector2.Zero, color));
+            _textComponent.transform.setLocalScale(_scale);
 
             return this;
         }
